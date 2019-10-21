df31a8adb4b9b258ebb2904c7004bd3820a2dfc7
Jovert Palonpon <palonponjovertlota@gmail.com>
2019-10-22 00:25:15 +0800

    wip

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,19 +1,19 @@
 import React from 'react'
 
 function TodoItem(props) {
-    return (
-        <div className="border-b py-2 flex items-center">
-            <input
-                type="checkbox"
-                className="mr-2"
-                checked={props.todo.completed}
-                onChange={() => props.onToggleComplete(props.todo.id)} />
+  return (
+    <div className="border-b py-2 flex items-center">
+      <input
+        type="checkbox"
+        className="mr-2"
+        checked={props.todo.completed}
+        onChange={() => props.onToggleComplete(props.todo.id)} />
 
-            <p className="text-gray-700">
-                {props.todo.title}
-            </p>
-        </div>
-    )
+      <p className="text-gray-700">
+        {props.todo.title}
+      </p>
+    </div>
+  )
 }
 
 export default TodoItem
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -2,17 +2,17 @@ import React from 'react'
 import TodoItem from './TodoItem'
 
 function TodoList(props) {
-    if (props.todos.length === 0) {
-        return <p className="text-gray-700">Nothing to do, Add now...</p>
-    }
+  if (props.todos.length === 0) {
+    return <p className="text-gray-700">Nothing to do, Add now...</p>
+  }
 
-    return (
-        <div>
-            {props.todos.map((todo, i) => (
-                <TodoItem key={i} todo={todo} onToggleComplete={props.onToggleComplete} />
-            ))}
-        </div>
-    )
+  return (
+    <div>
+      {props.todos.map((todo, i) => (
+        <TodoItem key={i} todo={todo} onToggleComplete={props.onToggleComplete} />
+      ))}
+    </div>
+  )
 }
 
 export default TodoList
