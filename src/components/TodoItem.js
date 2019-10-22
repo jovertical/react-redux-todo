42653d95f66f6aefa7f9cf87b65e1e07c61e6e14
Jovert Palonpon <palonponjovertlota@gmail.com>
2019-10-22 20:24:18 +0800

    Integrate PropTypes

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,19 +1,25 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
-function TodoItem(props) {
+function TodoItem({ todo, onToggleComplete }) {
   return (
     <div className="border-b py-2 flex items-center">
       <input
         type="checkbox"
         className="mr-2"
-        checked={props.todo.completed}
-        onChange={() => props.onToggleComplete(props.todo.id)} />
+        checked={todo.completed}
+        onChange={() => onToggleComplete(todo.id)} />
 
-      <p className="text-gray-700">
-        {props.todo.title}
+      <p className={`text-gray-700 ${todo.completed ? 'line-through' : ''}`}>
+        {todo.title}
       </p>
     </div>
   )
+}
+
+TodoItem.propTypes = {
+  todo: PropTypes.object,
+  onToggleComplete: PropTypes.func
 }
 
 export default TodoItem
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,18 +1,24 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import TodoItem from './TodoItem'
 
-function TodoList(props) {
-  if (props.todos.length === 0) {
+function TodoList({ todos, onToggleComplete }) {
+  if (todos.length === 0) {
     return <p className="text-gray-700">Nothing to do, Add now...</p>
   }
 
   return (
     <div>
-      {props.todos.map((todo, i) => (
-        <TodoItem key={i} todo={todo} onToggleComplete={props.onToggleComplete} />
+      {todos.map((todo, i) => (
+        <TodoItem key={i} todo={todo} onToggleComplete={onToggleComplete} />
       ))}
     </div>
   )
+}
+
+TodoList.propTypes = {
+  todos: PropTypes.array,
+  onToggleComplete: PropTypes.func
 }
 
 export default TodoList
